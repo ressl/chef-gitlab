1d67a8492d70cbe231a2044fb462b2b1ec642ef0
Robert Ressl <r.ressl@safematix.com>
2016-11-02 14:41:12 +0100

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,4 +2,4 @@ default['build-essential']['compile_time'] = true
 default['gitlab']['server']['url'] = nil
 default['gitlab']['server']['api'] = 'api/v3/projects'
 default['gitlab']['auth']['token_name'] = 'PRIVATE-TOKEN'
-default['gitlab']['auth']['token'] = 'RyPjAFMV3hJs9wZdBbbe'
+default['gitlab']['auth']['token'] = '*'
